5088912b608cd41bd23609d9ad833183d16844df
Rick Heist <rick.heist@gmail.com>
2023-06-14 23:03:42 -0400

    started fleshing out routes. categories .get by id is not working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,48 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const allCategories = await Category.findAll({
+      // include: 
+    });
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findbyPk(req.params.id, {
+        //include:
+    })
+    if (!allCategories) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const createCategory = await Category.create(req.body, {
+      // id: req.body.id
+      
+    });
+    res.status(200).json(createCategory);
+  }catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
