8d0652f6037243df26d7b4772e8e40fab0987e70
Rick Heist <rick.heist@gmail.com>
2023-06-18 10:59:16 -0400

    fixed all route bugs. routes and models are functioning

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -70,7 +70,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     console.log(deleteTag);
-  if (!deleteTagho) {
+  if (!deleteTag) {
     res.status(404).json({ message: 'No category found with that id'})
   }
     res.status(200).json(deleteTag)
